e0aa476403763c4ac1098e03424a6db9405cb232
Khai King <khaich@vtm.co.jp>
2020-03-14 13:34:39 +0700

    Create Project

---
+++ init-firebase.js
@@ -0,0 +1,15 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDAXoblPhIBk6Y4PAU3KUxWdzMVfAkjhas",
+    authDomain: "oritoeic-16a58.firebaseapp.com",
+    databaseURL: "https://oritoeic-16a58.firebaseio.com",
+    projectId: "oritoeic-16a58",
+    storageBucket: "oritoeic-16a58.appspot.com",
+    messagingSenderId: "413709750552",
+    // appId: "app-id",
+    // measurementId: "G-measurement-id",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.storage();
